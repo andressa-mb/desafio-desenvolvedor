6d28f08a2ce46fbb46a38f76dd6851d967ab12b3
andressa-mb <andressamesquita26@gmail.com>
2026-03-22 23:26:59 -0300

    models ref Files

--- teste/app/Models/File.php
+++ teste/app/Models/File.php
@@ -2,13 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class File extends Model
 {
+    protected $connection = 'mongodb';
+    protected $collection = 'files';
+
     protected $fillable = [
-        'original_name', 'path', 'hash_name', 'extension', 'size', 'user_id'
+        'original_name', 'path', 'hash_name', 'extension', 'size', 'user_id', 'status'
     ];
 
     protected function casts(): array {
@@ -20,5 +24,9 @@ class File extends Model
 
     public function user(): BelongsTo{
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function fileData(): HasMany{
+        return $this->hasMany(FileData::class, 'file_id', 'id');
     }
 }
