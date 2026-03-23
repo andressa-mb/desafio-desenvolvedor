69dd9d011436fb876ae25f0468a0e3eb1bd967a1
andressa-mb <andressamesquita26@gmail.com>
2026-03-23 00:08:06 -0300

    rotas

--- teste/routes/api.php
+++ teste/routes/api.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Files\FileController;
+use App\Http\Controllers\Files\FileDataController;
 
 Route::prefix('auth')->group(function () {
-    //login e register não precisam autenticação
     Route::post('/create', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -13,8 +13,8 @@ Route::prefix('auth')->group(function () {
     Route::get('/users', [AuthController::class, 'index']);
 });
 
-Route::prefix('docs')->middleware('auth:sanctum')->group(function () {
+Route::prefix('docs')->middleware('auth:api')->group(function () {
     Route::post('/save', [FileController::class, 'store']);
-    Route::get('/history', [FileController::class, 'history']);
-    Route::get('/search/{text}', [FileController::class, 'search']);
+    Route::get('/history-files', [FileController::class, 'historyFiles']);
+    Route::get('/search-file-data', [FileDataController::class, 'searchFileData']);
 });
